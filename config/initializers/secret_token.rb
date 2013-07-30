9ebb4c0de29e93eac92c4f1cc633d1c548685516
Toru Tamura <torut@ttsoft.jp>
2013-07-30 16:38:36 +0900

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kancole::Application.config.secret_token = 'ee1554e4544f3c1dad9be4bf8905db9eae4f08f68676a130dd624f2127e6098e7c444db8d16e4191cd7ddf32b24129ed17cf62da9df9651ede91c18d3bc41d26'
